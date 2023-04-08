fd808725ce98008e68eea03ff83c61e4c065ef65
Hanbin Tan <48960067+hanbintan2003@users.noreply.github.com>
2023-04-08 02:23:17 -0700

    Delete cmake-build-debug directory

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ice/Projects/TicTacToeSFML
-# Build directory: /Users/ice/Projects/TicTacToeSFML/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("_deps/googletest-build")
-subdirs("_deps/sfml-build")
--- cmake-build-debug/_deps/sfml-build/src/SFML/Window/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ice/Projects/TicTacToeSFML/cmake-build-debug/_deps/sfml-src/src/SFML/Window
-# Build directory: /Users/ice/Projects/TicTacToeSFML/cmake-build-debug/_deps/sfml-build/src/SFML/Window
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
